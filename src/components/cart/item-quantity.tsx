0f120d8b7d691a49c737fef130869cc6ad1b8202
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-06-11 19:39:49 -0300

    feat: implement CartItem and CartItemQuantity components; update CartSideBar to display cart items and subtotal

---
+++ src/components/cart/item-quantity.tsx
@@ -0,0 +1,9 @@
+import { Cart } from "@/types/cart";
+
+type Props = {
+    cartItem: Cart;
+};
+
+export const CartItemQuantity = ({ cartItem }: Props) => {
+    return <div>{cartItem.quantity}</div>;
+};
--- src/components/cart/sidebar.tsx
+++ src/components/cart/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     Sheet,
     SheetContent,
@@ -8,29 +10,47 @@ import {
 import { Button } from "@/components/ui/button";
 import { RocketIcon } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { useCartStore } from "@/stores/cart-store";
+import { CartItem } from "./item";
 
 export const CartSideBar = () => {
+    const { cart } = useCartStore((state) => state);
+
+    let subtotal = 0;
+    for (let item of cart) {
+        subtotal += item.quantity * item.product.price;
+    }
+
     return (
         <Sheet>
             <SheetTrigger asChild>
-                <Button>
+                <Button className="relative">
                     <RocketIcon className="mr-2" />
                     <p>Carrinho</p>
+                    {cart.length > 0 && (
+                        <div className="absolute size-3 bg-red-600 rounded-full -right-1 -top-1"></div>
+                    )}
                 </Button>
             </SheetTrigger>
-            <SheetContent>
+            <SheetContent className="px-5">
                 <SheetHeader>
                     <SheetTitle>Carrinho</SheetTitle>
                 </SheetHeader>
-                <div className="flex flex-col gap-5 my-3">...</div>
+                <div className="flex flex-col gap-5 my-3">
+                    {cart.map((item) => (
+                        <CartItem key={item.product.id} item={item} />
+                    ))}
+                </div>
                 <Separator className="my-4" />
                 <div className="flex justify-between items-center text-xs">
                     <div>Subtotal:</div>
-                    <div>...</div>
+                    <div>R$ {subtotal.toFixed(2)}</div>
                 </div>
                 <Separator className="my-4" />
                 <div className="text-center">
-                    <Button>Finalizar Compra</Button>
+                    <Button disabled={cart.length === 0}>
+                        Finalizar Compra
+                    </Button>
                 </div>
             </SheetContent>
         </Sheet>
